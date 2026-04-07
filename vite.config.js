c37854b434d287500a6f02a8d83b3888f63609d1
Daniel Hudak <daniel@MacBook-Pro-8.local>
2026-04-07 11:27:23 +0200

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Necessary for deployment on static hosting setups like GitHub Pages
+  base: '/marketing-dashboard-sto/', // Must match the GitHub repo name for GitHub Pages
 })
